38a7f8c530832ed34ddb6c214099ebcccbb6b87c
Mastrippolito Lorenzo <mastrippolito.lorenz@osdbsesto>
2026-05-11 13:23:08 +0200

    [ADD] aggiunti file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.progetto_informatica {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.progetto_informatica to javafx.fxml;
+    exports com.example.progetto_informatica;
+}
